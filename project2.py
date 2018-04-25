dfef4c83fb05e6f309996547c7809ee9252401d9
sourabh2k15 <sourab.reddy2k14@gmail.com>
2018-04-25 02:58:37 -0400

    GPR server ke lie

--- project2.py
+++ project2.py
@@ -3,34 +3,53 @@
 
 # # ML Project2
 
-# In[9]:
+# In[120]:
 
-# utility methods
+"""
 
+Utility methods
+
+"""
+
+# do PCA to reduce dimensionality, required for RVM and GPR
 def reduce_dimensions(data):
     pca = decomposition.PCA(n_components = 7)
     
     X = pca.fit_transform(data)
     return X
 
+# reads model from pickled object file
 def readObj(name):
     with open(name, 'rb') as input:
         clf = pickle.load(input)
     
     return clf
 
+# writes model to a pickled object file
 def writeObj(name, obj):
     with open(name, 'wb') as output:
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+        
+def zipData(X, Y):
+    return zip(X, Y)
+
+def unzipData(X):
+    x, y = zip(*X)
+
+    x = np.array(list(x))
+    y = np.array([np.where(output == 1)[0][0] for output in list(y)])
+    
+    return x,y
 
 
-# In[10]:
+# In[133]:
 
 '''
 
 loading input - output data and explore the dataset
 
 '''
+
 from scipy.io import loadmat
 from sklearn import decomposition
 
@@ -44,14 +63,11 @@ inputData  = inputDataObj['Proj2FeatVecsSet1']
 outputData = outputDataObj['Proj2TargetOutputsSet1']
 
 
-data = zip(inputData, outputData)
-
-
-# In[11]:
+# In[134]:
 
 """
 
-computes confusion matrix
+pretty prints confusion matrix and returns confusion matrix and accuracy score
 
 @param   Y                   predicted labels
 
@@ -77,10 +93,23 @@ def MyConfusionMatrix(Y, ClassNames):
     print "\nconfusion matrix: \n"
     print df
     
+    print "\n"
+    print "accuracy: ", accuracy
+    
     return conf_matrix, accuracy
 
 
-# In[12]:
+# In[135]:
+
+"""
+SVM (Support Vector Machine):
+
+performs grid search to compute optimal hyper-parameters
+uses those hyper-parameters for the estimator, fits it on the training data
+
+returns trained model and writes it to file for transfer learning
+
+"""
 
 def SVM(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train):
 
@@ -98,6 +127,7 @@ def SVM(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train):
     clf = estimator.best_estimator_
     
     print "found best hyperparameters:"
+
     print estimator.best_params_
     print "training the estimator"
 
@@ -113,7 +143,18 @@ def SVM(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train):
         print clf.score(X_validate, Y_validate)
 
 
-# In[13]:
+# In[136]:
+
+"""
+RVM (Relevance Vector Machine):
+
+uses PCA to reduce dimensionality as RVM training takes a long time
+also uses a subset of training data to save time
+
+
+returns trained model and writes it to file for transfer learning
+
+"""
 
 def RVM(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train):
     clf = OneVsRestClassifier(RVC(n_iter=1))
@@ -123,17 +164,29 @@ def RVM(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train):
     X_validate_reduced = reduce_dimensions(X_validate)
     
     if train:
-        clf.fit(X_train_reduced[:5000, :], Y_train[:5000])
+        clf.fit(X_train_reduced[:200, :], Y_train[:200])
         writeObj('rvm_model.pkl', clf)
+        
+        Y_pred = clf.predict(X_validate)
+        return Y_pred, clf
     else:
         clf = readObj('rvm_model.pkl')
         print clf.score(X_validate_reduced, Y_validate)
 
-    print time.clock() - start, "s"
-    print clf.predict_proba(X_validate[0])
+    print "training took ", time.clock() - start, "s"
 
 
-# In[14]:
+# In[ ]:
+
+"""
+GPR (Gaussian Process Regressor):
+
+uses PCA to reduce dimensionality as training takes a long time
+also uses a subset of training data to save time
+
+returns trained model and writes it to file for transfer learning
+
+"""
 
 from sklearn.multiclass import OneVsOneClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
@@ -141,26 +194,31 @@ from sklearn.gaussian_process.kernels import RBF
 from sklearn.multiclass import OneVsRestClassifier
 import pickle
 
-def Gaussian(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train):
-    print "GPR:"
+def GPR(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train):
+    print "GPR training :"
     
     X_train_reduced = reduce_dimensions(X_train)
     X_validate_reduced = reduce_dimensions(X_validate)
     
     if train:
+        start = time.clock()
         kernel_rbf = 1.0 * RBF()
-
+        
         clf = GaussianProcessClassifier(kernel=kernel_rbf, multi_class='one_vs_rest')
         clf.fit(X_train_reduced[:1000, :], Y_train[:1000])
 
-        writeObj('gaussian_model', clf)
+        writeObj('gaussian_model.pkl', clf)
+        print "training took ", time.clock() - start, " s"
+        
+        Y_pred = clf.predict(X_validate_reduced)
+        return Y_pred, clf
     else:
-        clf = readObj('gaussian_model')
+        clf = readObj('gaussian_model.pkl')
 
-        print clf.score(X_validate_reduced[:500, :], Y_validate[:500])
+        print clf.score(X_validate_reduced[:1000, :], Y_validate[:1000])
 
 
-# In[15]:
+# In[ ]:
 
 import numpy as np
 import time
@@ -172,23 +230,16 @@ from sklearn.model_selection import GridSearchCV
 
 def MyTrainClassifier(XEstimate, XValidate, Parameters):
     
-    X_train, Y_train = zip(*XEstimate)
-
-    X_train = np.array(list(X_train))
-    Y_train = np.array([np.where(output == 1)[0][0] for output in list(Y_train)])
+    X_train, Y_train = unzipData(XEstimate)
 
     # sampling a small amount of training data for finding optimal hyper-parameters
     X_hyper = X_train[:500, :]
     Y_hyper = Y_train[:500]
 
-    X_validate, Y_validate = zip(*XValidate)
+    X_validate, Y_validate = unzipData(XValidate)
 
-    X_validate = np.array(list(X_validate))
-    Y_validate = np.array([np.where(output == 1)[0][0] for output in list(Y_validate)])
-    
     train = Parameters['training_mode']
     
-    # SVM
     if Parameters['algorithm'] == 'SVM':
 
         Y_predict, model = SVM(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train)
@@ -199,16 +250,48 @@ def MyTrainClassifier(XEstimate, XValidate, Parameters):
 
     elif Parameters['algorithm'] == 'GPR':
 
-        Y_predict, model = Gaussian(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train)
+        Y_predict, model = GPR(X_hyper, Y_hyper, X_train, Y_train, X_validate, Y_validate, train)
 
-    return Y_predict, {'model' : model}
+    return Y_predict, {'model' : model, 'algorithm' : Parameters['algorithm']}
 
 
-# In[16]:
+# In[ ]:
 
 """
 
- K-fold cross validation script
+takes in XTest ( which is a zipped form of input and output data tuples ) and a trained model
+evaluates the performance of the model
+
+"""
+def TestMyClassifier(XTest, EstParameters):
+    model = EstParameters['model']
+    
+    Xactual, Yactual = unzipData(XTest)
+    Ypred = []
+    
+    algorithm = EstParameters['algorithm']
+    
+    if algorithm == 'GPR' or algorithm ==  'RVM':
+        X_actual = reduce_dimensions(X_actual)
+        
+    for x in X:
+        probabilities  = model.predict_proba(X)
+        max_probabilty = max(p)
+        
+        print probabilities
+        
+        y = p.index(max_probability)
+        Ypred.append(y)
+        
+    return Ypred
+
+
+# In[ ]:
+
+"""
+
+performs K-fold cross validation and selects the best model to prevent overfitting
+returns the array of confusion matrices and estimated parameter models for every fold
 
 """
 from sklearn.model_selection import KFold
@@ -249,11 +332,13 @@ def MyCrossValidate(XTrain, Nf):
     best_model_idx = accuracies.index(max(accuracies))
     best_model = EstParameters[best_model_idx]['model']
     
-    X, Y = zip(*XTrain)
+    X, Y = unzipData(XTrain)
     
-    X = np.array(list(X))
-    Y = np.array([np.where(output == 1)[0][0] for output in list(Y)])
+    algorithm = EstParameters[best_model_idx]['algorithm']
     
+    if algorithm == 'GPR' or algorithm ==  'RVM':
+        X = reduce_dimensions(X)
+        
     YTrain = best_model.predict(X)
     
     print "overall confusion matrix :"
@@ -263,12 +348,9 @@ def MyCrossValidate(XTrain, Nf):
     return YTrain, EstParameters, EstConfMatrices, ConfMatrix
 
 
-# In[17]:
-
-MyCrossValidate(data, 5)
-
-
 # In[ ]:
 
+data = zipData(inputData, outputData)
 
+MyCrossValidate(data, 5)
 
